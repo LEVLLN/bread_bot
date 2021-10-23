cf526379f391eaa575863090401cae6dc1e70c8e
Кудряшов Лев <lkudryashov@beeline.ru>
2021-10-23 17:23:32 +0300

    bread service handler testing 100%

--- bread_bot/tests/test_telegramer/test_services/test_bread_service_handler.py
+++ bread_bot/tests/test_telegramer/test_services/test_bread_service_handler.py
@@ -4,10 +4,13 @@ import unittest
 from unittest import mock
 
 from httpx import Response
+from sqlalchemy import and_
 
-from bread_bot.telegramer.models import LocalMeme, Chat, Property
+from bread_bot.telegramer.models import LocalMeme, Chat, Property, Member, \
+    Stats
+from bread_bot.telegramer.schemas.api_models import ForismaticQuote
 from bread_bot.telegramer.schemas.telegram_messages import \
-    StandardBodySchema, MemberSchema
+    StandardBodySchema, MemberSchema, VoiceSchema
 from bread_bot.telegramer.services.bread_service_handler import \
     BreadServiceHandler
 from bread_bot.telegramer.services.telegram_client import TelegramClient
@@ -339,7 +342,7 @@ class BuildMessageTestCase(unittest.IsolatedAsyncioTestCase):
             last_name='Martin',
             username='somebill'
         )
-        get_members_mock.return_value = [test_member, test_member_two]
+        get_members_mock.return_value = [test_member, test_member_two, None]
         message = self.default_message.message.copy(deep=True)
         handler = BreadServiceHandler(
             client=self.telegram_client,
@@ -356,6 +359,18 @@ class BuildMessageTestCase(unittest.IsolatedAsyncioTestCase):
         )
         self.assertIn(
             f'{test_member_two.first_name} {test_member_two.last_name}',
+            result,
+        )
+        self.assertTrue(result.startswith('Топ'))
+        self.assertIn(
+            'Some param',
+            result,
+        )
+        handler.chat_id = 111111
+        handler.params = 'Some param'
+        result = await handler.top()
+        self.assertIn(
+            f'{message.source.first_name} {message.source.last_name}',
             result,
         )
         self.assertTrue(result.startswith('Топ'))
@@ -412,3 +427,355 @@ class BuildMessageTestCase(unittest.IsolatedAsyncioTestCase):
             len(handler.params),
         )
 
+    async def test_change_local_meme(self):
+        handler = BreadServiceHandler(
+            client=self.telegram_client,
+            message=self.default_message.message,
+            db=self.session,
+            is_edited=False,
+        )
+        handler.params = 'key1=value1'
+        # ADD
+        result = await handler.add_local_meme_name()
+        self.assertEqual(
+            result,
+            'Сделал'
+        )
+        local_meme = await LocalMeme.async_first(
+            db=self.session,
+            where=and_(
+                LocalMeme.chat_id == handler.chat_id,
+                LocalMeme.type == LocalMemeTypesEnum.MEME_NAMES.name,
+            ),
+        )
+        self.assertEqual(
+            local_meme.data['key1'],
+            ['value1', ]
+        )
+        # SHOW
+        handler.params = 'key1'
+        result = await handler.show_local_meme_names()
+        self.assertIn(
+            'key1',
+            result,
+        )
+        # DELETE
+        handler.params = 'key1'
+        result = await handler.delete_local_meme_name()
+        self.assertEqual(
+            result,
+            'Сделал'
+        )
+        local_meme = await LocalMeme.async_first(
+            db=self.session,
+            where=and_(
+                LocalMeme.chat_id == handler.chat_id,
+                LocalMeme.type == LocalMemeTypesEnum.FREE_WORDS.name,
+            ),
+        )
+        self.assertNotIn(
+            'key1',
+            local_meme.data,
+        )
+
+    async def test_change_substring(self):
+        handler = BreadServiceHandler(
+            client=self.telegram_client,
+            message=self.default_message.message,
+            db=self.session,
+            is_edited=False,
+        )
+        # ADD
+        handler.params = 'key1=value1'
+        result = await handler.add_local_substring()
+        self.assertEqual(
+            result,
+            'Сделал'
+        )
+        local_meme = await LocalMeme.async_first(
+            db=self.session,
+            where=and_(
+                LocalMeme.chat_id == handler.chat_id,
+                LocalMeme.type == LocalMemeTypesEnum.SUBSTRING_WORDS.name,
+            ),
+        )
+        self.assertEqual(
+            local_meme.data['key1'],
+            ['value1', ]
+        )
+        # SHOW
+        handler.params = 'key1'
+        result = await handler.show_local_substrings()
+        self.assertIn(
+            'key1',
+            result,
+        )
+        # DELETE
+        handler.params = 'key1'
+        result = await handler.delete_local_substring()
+        self.assertEqual(
+            result,
+            'Сделал'
+        )
+        local_meme = await LocalMeme.async_first(
+            db=self.session,
+            where=and_(
+                LocalMeme.chat_id == handler.chat_id,
+                LocalMeme.type == LocalMemeTypesEnum.FREE_WORDS.name,
+            ),
+        )
+        self.assertNotIn(
+            'key1',
+            local_meme.data,
+        )
+
+    async def test_change_unknown_answer(self):
+        handler = BreadServiceHandler(
+            client=self.telegram_client,
+            message=self.default_message.message,
+            db=self.session,
+            is_edited=False,
+        )
+        # ADD
+        handler.params = 'value1'
+        result = await handler.add_unknown_answer()
+        self.assertEqual(
+            result,
+            'Ура! У группы появились Фразы на отсутствующие команды'
+        )
+        local_meme = await LocalMeme.async_first(
+            db=self.session,
+            where=and_(
+                LocalMeme.chat_id == handler.chat_id,
+                LocalMeme.type == LocalMemeTypesEnum.UNKNOWN_MESSAGE.name,
+            ),
+        )
+        self.assertEqual(
+            local_meme.data,
+            ['value1', ]
+        )
+
+    async def test_change_free_word(self):
+        handler = BreadServiceHandler(
+            client=self.telegram_client,
+            message=self.default_message.message,
+            db=self.session,
+            is_edited=False,
+        )
+        # ADD
+        handler.params = 'key1=value1'
+        result = await handler.add_local_free_word()
+        self.assertEqual(
+            result,
+            'Сделал'
+        )
+        local_meme = await LocalMeme.async_first(
+            db=self.session,
+            where=and_(
+                LocalMeme.chat_id == handler.chat_id,
+                LocalMeme.type == LocalMemeTypesEnum.FREE_WORDS.name,
+            ),
+        )
+        self.assertEqual(
+            local_meme.data['key1'],
+            ['value1', ]
+        )
+        # SHOW
+        handler.params = 'key1'
+        result = await handler.show_local_free_words()
+        self.assertIn(
+            'key1',
+            result,
+        )
+        # DELETE
+        handler.params = 'key1'
+        result = await handler.delete_local_free_word()
+        self.assertEqual(
+            result,
+            'Сделал'
+        )
+        local_meme = await LocalMeme.async_first(
+            db=self.session,
+            where=and_(
+                LocalMeme.chat_id == handler.chat_id,
+                LocalMeme.type == LocalMemeTypesEnum.FREE_WORDS.name,
+            ),
+        )
+        self.assertNotIn(
+            'key1',
+            local_meme.data,
+        )
+
+    @mock.patch('bread_bot.telegramer.services.bread_service_handler.'
+                'ForismaticClient.get_quote_text')
+    async def test_get_quote(self, get_quote_mock: mock.Mock):
+        get_quote_mock.return_value = ForismaticQuote(
+            text='Some text',
+            author='Some author'
+        )
+        member = await Member.async_add_by_kwargs(
+            db=self.session,
+            username='user1',
+            first_name='first_name_test',
+            last_name='last_name_test'
+        )
+        chat = await Chat.async_add_by_kwargs(
+            db=self.session,
+            chat_id=55551,
+            name='Some chat'
+        )
+        handler = BreadServiceHandler(
+            client=self.telegram_client,
+            message=self.default_message.message,
+            db=self.session,
+            is_edited=False,
+        )
+        handler.chat_db = chat
+        handler.member_db = member
+        handler.chat_id = chat.chat_id
+        result = await handler.get_quote()
+        self.assertEqual(
+            result,
+            'Some text\n\n© Some author'
+        )
+        stats = await Stats.async_first(
+            db=self.session,
+            where=and_(
+                Stats.member_id == member.id,
+                Stats.chat_id == chat.chat_id,
+            )
+        )
+        self.assertEqual(
+            stats.count,
+            1
+        )
+        get_quote_mock.assert_called_once()
+
+    @mock.patch('bread_bot.telegramer.services.telegram_client.'
+                'TelegramClient.send_voice')
+    async def test_send_to_voice(self, send_voice_mock: mock.Mock):
+        send_voice_mock.return_value = True
+        message = self.default_message.message.copy(deep=True)
+        message.voice = VoiceSchema(
+            duration=15,
+            file_id='some_file_id'
+        )
+        chat = await Chat.async_add_by_kwargs(
+            db=self.session,
+            chat_id=55552,
+            name='Some chat',
+        )
+        handler = BreadServiceHandler(
+            client=self.telegram_client,
+            message=message,
+            db=self.session,
+            is_edited=False,
+        )
+        handler.chat_db = chat
+        # Not available for sending
+        result = await handler.send_fart_voice()
+        self.assertFalse(result)
+        send_voice_mock.assert_not_called()
+        # Not property
+        chat.is_voice_trigger = True
+        chat = await Chat.async_add(
+            db=self.session,
+            instance=chat,
+        )
+        handler.chat_db = chat
+        self.assertTrue(chat.is_voice_trigger)
+        result = await handler.send_fart_voice()
+        self.assertFalse(result)
+        send_voice_mock.assert_not_called()
+        # Success with property
+        await Property.async_add_by_kwargs(
+            db=self.session,
+            slug=PropertiesEnum.BAD_VOICES.name,
+            data=['Some_file_id']
+        )
+        result = await handler.send_fart_voice()
+        self.assertTrue(result)
+        send_voice_mock.assert_called_once()
+        send_voice_mock.assert_called_once_with(
+            chat_id=handler.chat_id,
+            voice_file_id='Some_file_id',
+            reply_to=message.message_id,
+        )
+
+    async def test_set_edited_trigger(self):
+        chat = await Chat.async_add_by_kwargs(
+            db=self.session,
+            chat_id=55553,
+            name='Some chat',
+        )
+        handler = BreadServiceHandler(
+            client=self.telegram_client,
+            message=self.default_message.message,
+            db=self.session,
+            is_edited=False,
+        )
+        handler.chat_db = chat
+        self.assertFalse(
+            chat.is_edited_trigger,
+        )
+        # Enable flag
+        result = await handler.set_edited_trigger()
+        self.assertEqual(
+            result,
+            'Включено реагирование на редактирование сообщений'
+        )
+        chat = await Chat.async_first(
+            db=self.session,
+            where=Chat.id == chat.id,
+        )
+        self.assertTrue(chat.is_edited_trigger)
+        # Disable flag
+        result = await handler.set_edited_trigger()
+        self.assertEqual(
+            result,
+            'Выключено реагирование на редактирование сообщений'
+        )
+        chat = await Chat.async_first(
+            db=self.session,
+            where=Chat.id == chat.id,
+        )
+        self.assertFalse(chat.is_edited_trigger)
+
+    async def test_set_voice_trigger(self):
+        chat = await Chat.async_add_by_kwargs(
+            db=self.session,
+            chat_id=55554,
+            name='Some chat',
+        )
+        handler = BreadServiceHandler(
+            client=self.telegram_client,
+            message=self.default_message.message,
+            db=self.session,
+            is_edited=False,
+        )
+        handler.chat_db = chat
+        self.assertFalse(
+            chat.is_voice_trigger,
+        )
+        # Enable flag
+        result = await handler.set_voice_trigger()
+        self.assertEqual(
+            result,
+            'Включено реагирование на голосовые сообщения'
+        )
+        chat = await Chat.async_first(
+            db=self.session,
+            where=Chat.id == chat.id,
+        )
+        self.assertTrue(chat.is_voice_trigger)
+        # Disable flag
+        result = await handler.set_voice_trigger()
+        self.assertEqual(
+            result,
+            'Выключено реагирование на голосовые сообщения'
+        )
+        chat = await Chat.async_first(
+            db=self.session,
+            where=Chat.id == chat.id,
+        )
+        self.assertFalse(chat.is_voice_trigger)
