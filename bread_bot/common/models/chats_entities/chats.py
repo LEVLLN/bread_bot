18247d070aa1c3bc2b092201f219e6f6b084d8e5
Kudryashov Lev <kudryashov_ly@tochka.com>
2022-12-24 11:01:49 +0300

    Delete useless columns from chats table

--- bread_bot/common/models/chats_entities/chats.py
+++ bread_bot/common/models/chats_entities/chats.py
@@ -13,9 +13,6 @@ class Chat(mixins.AbstractIsActiveBaseModel, mixins.BaseModel, mixins.CRUDMixin)
 
     chat_id = Column(BigInteger, nullable=False, unique=True)
     name = Column(String(255), nullable=True)
-    is_edited_trigger = Column(Boolean, default=False, nullable=False)
-    is_voice_trigger = Column(Boolean, default=False, nullable=False)
-    answer_chance = Column(SmallInteger, default=100, nullable=False)
 
     members = relationship("ChatToMember", back_populates="chat")
     answer_packs = relationship("AnswerPacksToChats", back_populates="chats")
