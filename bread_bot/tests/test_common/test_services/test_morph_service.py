d1e2a7ec2b892329fa317c86208d9965b9f63fcb
Kudryashov Lev <kudryashov_ly@tochka.com>
2023-03-22 00:42:44 +0300

    Fix replace words with new lines in morph_text

--- bread_bot/tests/test_common/test_services/test_morph_service.py
+++ bread_bot/tests/test_common/test_services/test_morph_service.py
@@ -15,7 +15,6 @@ async def test_morph_text(db, dictionary_entity_factory, member_service, message
     assert await DictionaryEntity.async_filter(db, DictionaryEntity.chat_id == member_service.chat.id)
 
     result = await MorphService(db, chat_id=member_service.chat.id).morph_text(text)
-    print((result, ))
     assert result.startswith("Только до 1830 зариф вплотную прыгал")
     assert result.split()[-1] in ("головой", "головою")
 
