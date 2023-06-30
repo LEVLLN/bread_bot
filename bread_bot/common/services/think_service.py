90c28f8791e1e2e37cc8cf66d6d6a0b6907b6f0e
kireev <kireev_y@tochka.com>
2023-06-30 15:18:32 +0300

    CLOSES#53
    - add MAIN_PROMPT from env to chat gpt service

--- bread_bot/common/services/think_service.py
+++ bread_bot/common/services/think_service.py
@@ -1,3 +1,6 @@
+import os
+from functools import lru_cache
+
 from httpx import RequestError
 
 from bread_bot.common.clients.openai_client import get_chat_gpt_client
@@ -8,9 +11,14 @@ class ThinkService:
         client = get_chat_gpt_client()
         try:
             result = await client.get_chatgpt_answer(
-                "Будь мудр и эпичен при ответе",
+                await self.get_prompt(),
                 question,
             )
             return result
         except RequestError:
             return "Не могу думать, думалка не работает((("
+
+    @lru_cache
+    async def get_prompt(self) -> str:
+        # TODO: Нужна более гибкая система добавления промпта
+        return os.getenv("MAIN_PROMPT", "Будь мудр и эпичен при ответе")
