e036d3b1f017057c3bc98dc2994bb133b0493c26
Kudryashov Lev <kudryashov_ly@tochka.com>
2023-08-01 01:34:49 +0300

    Procrastinate testing inmemory

--- bread_bot/main/procrastinate.py
+++ bread_bot/main/procrastinate.py
@@ -1,10 +1,14 @@
-from procrastinate import AiopgConnector, App
+from procrastinate import AiopgConnector, App, testing
 
-from bread_bot.main.settings import DATABASE_SETTINGS
+from bread_bot.main.settings import DATABASE_SETTINGS, ENVIRONMENT
 
 db_settings = DATABASE_SETTINGS["default"]
+if ENVIRONMENT == "TESTING":
+    connector = testing.InMemoryConnector()
+else:
+    connector = AiopgConnector(host=db_settings["host"], user=db_settings["user"], password=db_settings["password"])
 app = App(
-    connector=AiopgConnector(host=db_settings["host"], user=db_settings["user"], password=db_settings["password"]),
+    connector=connector,
     import_paths=["bread_bot.common.async_tasks"],
 )
 app.open()
