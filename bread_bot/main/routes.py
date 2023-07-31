b5731a90793ad9dc4767b5f7fd7ca9a75106d556
Kudryashov Lev <kudryashov_ly@tochka.com>
2023-08-01 02:41:40 +0300

    Rm debug call tasks

--- bread_bot/main/routes.py
+++ bread_bot/main/routes.py
@@ -3,9 +3,6 @@ import logging
 from fastapi import APIRouter
 from starlette.requests import Request
 
-from bread_bot.common.async_tasks import async_free_promt
-from bread_bot.main.procrastinate import app
-
 logger = logging.getLogger(__name__)
 
 router = APIRouter(
@@ -16,7 +13,5 @@ router = APIRouter(
 
 @router.get("/")
 async def root(request: Request):
-    async with app.open_async():
-        await async_free_promt.defer_async(text="LOL", chat_id=1, reply_to_message_id=1)
     logger.info("Test log")
     return {"foo": "baz"}
